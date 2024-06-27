29c53974a80ff1851b6bd0b1a0f3fba14d780998
Aadarshk11 <aadarshkasturey@gmail.com>
2024-06-27 18:09:53 +0530

    added carousel and cards

--- myartapp/src/screens/Home.js
+++ myartapp/src/screens/Home.js
@@ -1,12 +1,31 @@
 import React from "react";
-import NavigationBar from '../components/NavigationBar';
-import Footer from '../components/footer';
+import NavigationBar from "../components/NavigationBar";
+import Footer from "../components/footer";
+import ArtCarousel from '../components/Carousel';
+import ArtCard from '../components/Card';
 
 export default function Home() {
   return (
     <div>
       <div>
-        <NavigationBar/>
+        <NavigationBar />
+      </div>
+      <div className="container mt-4">
+        
+        <ArtCarousel />
+
+        <h2 className="mt-4"></h2>
+        <div className="row">
+          <div className="col-md-4 mb-4">
+            <ArtCard />
+          </div>
+          <div className="col-md-4 mb-4">
+            <ArtCard />
+          </div>
+          <div className="col-md-4 mb-4">
+            <ArtCard />
+          </div>
+        </div>
       </div>
       <div>
         <Footer />
