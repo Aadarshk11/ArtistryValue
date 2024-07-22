07dfebedfeecc54b29f6801b24c1857aa1f36f25
Aadarshk11 <aadarshkasturey@gmail.com>
2024-07-22 19:19:28 +0530

    express installed and connected database to app using mongoose

---
+++ myartapp/backend/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const mongoDB = require('./mongoose');
+const app = express();
+const port = 5000;
+
+mongoDB();
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
